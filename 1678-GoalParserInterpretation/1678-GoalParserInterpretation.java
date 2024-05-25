c11b29304697d97f7041cbaf229c599277d65869
Ayan0209 <33597664+Ayan0209@users.noreply.github.com>
2024-05-24 18:58:55 -0700

    Create file

---
+++ 1678-GoalParserInterpretation/1678-GoalParserInterpretation.java
@@ -0,0 +1,13 @@
+            else if(command.charAt(index) == '(' && command.charAt(index+1) == ')'){
+                index++;
+            }
+        while(index<command.length()){
+            if(command.charAt(index) == 'G'){
+                result.append('G');
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+
+class Solution {
+    public String interpret(String command) {
+        
+"
