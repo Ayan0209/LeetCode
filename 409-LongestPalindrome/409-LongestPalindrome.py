e4c529602d6b6d5c088757e1f85f49c2c01e30a8
Ayan0209 <33597664+Ayan0209@users.noreply.github.com>
2024-05-26 14:00:39 -0700

    Create file

---
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -0,0 +1,12 @@
+
+        for char in char_count:
+
+        final_count = 0
+        odd_is_present = False
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+    def longestPalindrome(self, s: str) -> int:
+
+        char_count ={}
+class Solution:
+"
