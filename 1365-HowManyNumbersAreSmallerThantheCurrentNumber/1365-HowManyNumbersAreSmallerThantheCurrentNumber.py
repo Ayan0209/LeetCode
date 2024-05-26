95d9bce90667d226dbcbd4513c491d81c1c21494
Ayan0209 <33597664+Ayan0209@users.noreply.github.com>
2024-05-26 13:02:05 -0700

    Create file

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.py
@@ -0,0 +1,13 @@
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+
+                if j != i and nums[j]<nums[i]:
+
+            count = 0
+                    count+=1
+            
+            result.append(count)
+        
+        result = []
+        count = 0
+[
